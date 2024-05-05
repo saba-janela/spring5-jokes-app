104d020410b7bae1ebc2f4e33ec76594bb71b611
Saba Janelidze <sjanelidze02@gmail.com>
2024-05-05 16:42:30 +0400

    Added Chuck Norris Dependency. Closes #2

--- src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5JokesAppV2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5JokesAppV2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppV2Application.class, args);
+    }
 
 }
