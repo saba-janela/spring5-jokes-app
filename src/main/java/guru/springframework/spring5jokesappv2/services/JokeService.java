18d8e42f92173d3d64d57385b17caf521cf4ec79
Saba Janelidze <sjanelidze02@gmail.com>
2024-05-05 17:04:13 +0400

    Created A Joke Service. Closes #3

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface JokeService {
+
+    String getJoke();
+
+}
